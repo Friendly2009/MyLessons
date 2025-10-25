65b6d762cf49fe4bfdfa500fb97f4744f7414ef6
friendly2009 <kirill.tsyganov@internet.ru>
2025-10-25 21:26:43 +0300

    стер 2/3 контроллера что бы переписать по уму код, добавил авторизацию и кнопки в privacy возвращающую с моделью

--- MyLessons/Controllers/HomeController.cs
+++ MyLessons/Controllers/HomeController.cs
@@ -12,58 +12,36 @@ namespace MyLessons.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext context;
-        private string key = "12vui323x67r8wetfx0932";
-		private string count = "21763gr6uxqi78ygq3f";
+		private readonly string count = "aldhkvf";
+		user MyUser = new user();
 		public HomeController(ILogger<HomeController> logger, ApplicationDbContext _context)
         {
             _logger = logger;
             context = _context;
         }
 		public IActionResult Index()
-        {
-			HttpContext.Session.SetInt32(count, 0);
-			ViewBag.isReg = false;
+		{
 			return View();
-        }
-		public IActionResult ChooseClass(string one)
-		{
-            var text = context.data.FirstOrDefault(t => t.Id == user.id).text;
-
-            HttpContext.Session.SetString(key, json);
-
-            ViewBag.user = user;
-            ViewBag.Lessons = GetLessons(text);
-            ViewBag.social = socials(GetLessons(text));
-            ViewBag.one = GetLessons(text)[0].clas;
-            return View("privacy");
-        }
-        [HttpPost]
-		public IActionResult Privacy(user us)
-		{
-			HttpContext.Session.SetInt32(count, 0);
-			if (ModelState.IsValid)
-			{
-				ViewBag.isReg = true;
-				var user = context.user.FirstOrDefault(t => t.login == us.login);
-				var text = context.data.FirstOrDefault(t => t.Id == user.id).text;
-
-                if (user.password == us.password)
-				{
-					string json = JsonConvert.SerializeObject(user);
-					HttpContext.Session.SetString(key, json);
-
-                    ViewBag.user = user;
-					ViewBag.Lessons = GetLessons(text);
-					ViewBag.social = socials(GetLessons(text));
-					ViewBag.one = GetLessons(text)[0].clas;
-					return View("privacy");
-				}
-			}
-			return View("Index");
 		}
 		[HttpPost]
-        public IActionResult AddAccount(newuser newusers, string Newlogin, string Newpassword)
-        {
+		public IActionResult Privacy(user us)
+		{
+			if (!ModelState.IsValid)
+			{
+				ViewBag.name = us.login;
+				return View("Index", us);
+			}
+			ViewBag.name = us.login;
+			return View(us);
+		}
+		[HttpGet]
+		public IActionResult Choose(user us)
+		{
+			ViewBag.name = us.login;
+			return View("Privacy", us);
+		}
+		public IActionResult AddAccount(newuser newusers, string Newlogin, string Newpassword)
+		{
 			HttpContext.Session.SetInt32(count, 1);
 			if (ModelState.IsValid)
 			{
@@ -73,7 +51,7 @@ namespace MyLessons.Controllers
 				context.user.Add(us);
 				context.SaveChanges();
 			}
-			if(Newlogin == null && HttpContext.Session.GetInt32(count) == 1)
+			if (Newlogin == null && HttpContext.Session.GetInt32(count) == 1)
 			{
 				ViewBag.n = "¬ведите логин";
 			}
@@ -81,27 +59,58 @@ namespace MyLessons.Controllers
 			{
 				ViewBag.num = "¬ведите пароль";
 			}
-			return View("Index");
-        }
-		public List<string> socials(List<lesson> data)
+			return RedirectToAction("Privacy");
+		}
+		private List<lesson> ConvertToData(string data)
 		{
-			List<string> list = new List<string>();
-			for(int i = 0; i < data.Count;i++)
+			var list = new List<lesson>();
+			string[] lessons = data.Split('|');
+			for (int i = 0; i < lessons.Length; i++)
 			{
-				list.Add(data[i].clas);
+				list.Add(JsonConvert.DeserializeObject<lesson>(lessons[i]));
 			}
-			list.Distinct().ToList();
+			List<string> socials = new List<string>();
+			for (int i = 0; i < list.Count; i++)
+			{
+				socials.Add(Convert.ToString(list[i].clas));
+			}
+			socials = socials.Distinct().ToList();
+			ViewBag.socials = socials;
 			return list;
 		}
-		public List<lesson> GetLessons(string data)
+		[HttpPost]
+		private List<string> ConvertToTeachers(string data, int type)
 		{
-			List<lesson> list = new List<lesson>();
-			string[] lessonArray = data.Split('|');	
-			for(int i = 0; i < lessonArray.Length;i++)
+			List<string> Names = new List<string>();
+			List<string> Objects = new List<string>();
+			string[] Teachers = data.Split("|");
+			for (int i = 0; i < Teachers.Length; i++)
 			{
-				list.Add(JsonConvert.DeserializeObject<lesson>(lessonArray[i]));
+				string[] name = Teachers[i].Split("`");
+				Names.Add(name[0].Trim());
+				Objects.Add(name[1].Trim());
 			}
-			return list;
+			if (type == 0)
+			{
+				return Names;
+			}
+			else
+			{
+				return Objects;
+			}
+		}
+		private string FindLastClass(List<lesson> less)
+		{
+			string result = "";
+			for (int i = 0; i < less.Count; i++)
+			{
+				try
+				{
+					result = less[i - 1].clas;
+				}
+				catch { }
+			}
+			return result;
 		}
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
