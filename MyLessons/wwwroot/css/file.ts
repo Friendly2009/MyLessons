d6aea2f650dd29736d97ee1b35fa421010462b60
friendly2009 <kirill.tsyganov@internet.ru>
2025-11-01 18:46:17 +0300

    a just update

---
+++ MyLessons/wwwroot/css/file.ts
@@ -0,0 +1,16 @@
+﻿var Buttons_Star = document.querySelectorAll('.mx-1');
+for (let i = 0; i < Buttons_Star.length; i++) {
+    Buttons_Star[i].setAttribute("num", (i + 1).toString());
+}
+Buttons_Star.forEach(function (element) {
+    element.addEventListener('click', function () {
+        let count = Number(element.getAttribute("num"));
+        for (let i = 0; i < Buttons_Star.length; i++) {
+            Buttons_Star[i].setAttribute('src', "/css/star.png");
+        }
+        for (let i = 0; i < count; i++) {
+            Buttons_Star[i].setAttribute('src', "/css/start.png");
+        }
+/*        price.setAttribute('value', count);*/
+    });
+});
