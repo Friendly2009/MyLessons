c5b6a9757609f7cff1b3cc1d6e67b13b66ae8f6b
kirill.tsyganov@Internet.ru <kirill.tsyganov@Internet.ru>
2025-11-28 19:14:09 +0300

    update project

--- MyLessons/wwwroot/js/JavaScript.js
+++ MyLessons/wwwroot/js/JavaScript.js
@@ -1,5 +1,7 @@
 ﻿var Buttons_Star = document.querySelectorAll('.mx-1');
+var AddLesson_Button = document.getElementById('AddLesson');
 var reader = document.querySelectorAll('td');
+let Tracking_for_Add_Lesson = false;
 for (let i = 0; i < Buttons_Star.length; i++) {
     Buttons_Star[i].setAttribute("num", (i + 1).toString());
 }
@@ -12,16 +14,33 @@ Buttons_Star.forEach(function (element) {
         for (let i = 0; i < Number(count); i++) {
             Buttons_Star[i].setAttribute('src', "/css/start.png");
         }
-    //    price.setAttribute('value', count);
+        //    price.setAttribute('value', count);
     });
 });
 reader.forEach(td => {
     td.addEventListener("click", clickable => {
         SelectClick(td);
-        
+        if (Tracking_for_Add_Lesson === true) {
+            AddLessonInBase(td);
+        }
     })
 });
 
+AddLesson_Button.addEventListener("click", clicable => {
+    Tracking_for_Add_Lesson = true;
+});
+
+function AddLessonInBase(td) {
+    Tracking_for_Add_Lesson = false;
+    var ItemForNewLesson = td.querySelectorAll('Input');
+    let Newday = document.getElementsByName('Newday');
+    let Newnumber = document.getElementsByName('Newnumber');
+    let Newless = document.getElementsByName('Newless');
+    let Newteacher = document.getElementsByName('Newteacher');
+    let Newroom = document.getElementsByName('Newroom');
+    let Newclas = document.getElementsByName('Newclas');
+
+}
 
 function SelectClick(td) {
     for (let i = 0; i < reader.length; i++) {
