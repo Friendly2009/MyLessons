663e158065a8ca9eb9fd4e550edc7ede04513c47
S <kirill.tsyganov@internet.ru>
2026-01-16 17:32:13 +0300

    добавлена боковая панель для отображения информации пользователя

---
+++ MyLessons/wwwroot/js/sidebar.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar");
+    const toggleBtn = document.getElementById("toggleSidebar");
+
+    let isOpen = false;
+
+    toggleBtn.addEventListener("click", function () {
+        if (!isOpen) {
+            sidebar.style.left = "0px";
+            toggleBtn.style.left = "500px";
+            isOpen = true;
+        } else {
+            sidebar.style.left = "-500px";
+            toggleBtn.style.left = "0px";
+            isOpen = false;
+        }
+    });
+});
